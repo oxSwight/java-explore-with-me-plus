b841acd64a0d86f730f1ae8ae907079a7d112d5c
Aleksei <andreev@almaz.spb.ru>
2025-06-19 15:16:04 +0300

    Исправлял ошибки #2

--- ewm-main-service/src/main/java/ru/practicum/explore/global/controller/ExceptionController.java
+++ ewm-main-service/src/main/java/ru/practicum/explore/global/controller/ExceptionController.java
@@ -2,7 +2,6 @@ package ru.practicum.explore.global.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ru.practicum.explore.common.exception.*;
