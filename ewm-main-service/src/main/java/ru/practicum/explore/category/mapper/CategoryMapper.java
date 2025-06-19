ffd698a54d0d4536f0ab262ea8ef613a0e19b54d
Aleksei <andreev@almaz.spb.ru>
2025-06-19 15:46:03 +0300

    Исправлял ошибки #4

--- ewm-main-service/src/main/java/ru/practicum/explore/category/mapper/CategoryMapper.java
+++ ewm-main-service/src/main/java/ru/practicum/explore/category/mapper/CategoryMapper.java
@@ -7,9 +7,5 @@ import ru.practicum.explore.category.dto.*;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CategoryMapper {
 
-    /* NewCategoryDto → внутренний CategoryDto (используется сервисом) */
     CategoryDto toCategoryDto(NewCategoryDto src);
-
-    /* CategoryDtoWithId → то же самое без изменений (удобно для контроллера) */
-    CategoryDtoWithId toCategoryDtoWithId(CategoryDtoWithId src);
 }
