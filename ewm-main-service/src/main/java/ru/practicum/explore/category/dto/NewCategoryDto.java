2ba01121d996c54685c0d7d89489c27a8ce54b3a
Aleksei <andreev@almaz.spb.ru>
2025-06-19 15:46:03 +0300

    Исправлял ошибки #4

--- ewm-main-service/src/main/java/ru/practicum/explore/category/dto/NewCategoryDto.java
+++ ewm-main-service/src/main/java/ru/practicum/explore/category/dto/NewCategoryDto.java
@@ -5,7 +5,8 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 public class NewCategoryDto {
     @NotBlank
     @Size(min = 1, max = 50)
